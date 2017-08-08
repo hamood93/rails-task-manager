ecf61b83352ba7bc07bd4f528e48f2ebff77f8e2
mohamed almoaakad <mohamedmoaakad93mm@gmail.com>
2017-08-08 17:07:14 +0100

    Task_manager

--- db/migrate/20170808132920_create_tasks.rb
+++ db/migrate/20170808132920_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.integer :priority
+      t.string :title
+      t.string :message
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808114429) do
+ActiveRecord::Schema.define(version: 20170808132920) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
